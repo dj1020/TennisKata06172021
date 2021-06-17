38d1272927dc084459b1f018061465f9ce85ec11
Ken Lin <kenlin@kkbox.com>
2021-06-18 04:53:01 +0800

    First try, only result

--- Tennis.php
+++ Tennis.php
@@ -2,16 +2,122 @@
 
 class Tennis
 {
+    private $firstPlayerScore  = 0;
+    private $secondPlayerScore = 0;
+    private $firstPlayerName;
+    private $secondPlayerName;
+    private $scoreText         = [
+        0 => 'Love',
+        1 => 'Fifteen',
+        2 => 'Thirty',
+        3 => 'Forty',
+    ];
 
     /**
      * Tennis constructor.
      */
-    public function __construct()
+    public function __construct($firstPlayerName, $secondPlayerName)
     {
+        $this->firstPlayerName = $firstPlayerName;
+        $this->secondPlayerName = $secondPlayerName;
     }
 
     public function getResult()
     {
-        return 'Love All';
+        if ($this->isScoreDifferent()) {
+            if ($this->isNextScoreGamePoint()) {
+                return $this->getAdvPlayer() . ($this->isAdv() ? ' adv.' : ' Win.');
+            }
+            return $this->lookUpScore();
+        }
+
+        if ($this->isDeuce()) {
+            return $this->deuce();
+        }
+
+        return $this->sameScore();
     }
+
+    public function givenFirstPlayerScoreTimes(int $int)
+    {
+        for ($i = 0; $i < $int; $i++) {
+            $this->firstPlayerScore++;
+        }
+    }
+
+    public function givenSecondPlayerScoreTimes(int $int)
+    {
+        for ($i = 0; $i < $int; $i++) {
+            $this->secondPlayerScore++;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getAdvPlayer()
+    {
+        $advPlayerName = ($this->firstPlayerScore > $this->secondPlayerScore)
+            ? $this->firstPlayerName
+            : $this->secondPlayerName;
+
+        return $advPlayerName;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isAdv(): bool
+    {
+        return abs($this->firstPlayerScore - $this->secondPlayerScore) == 1;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isNextScoreGamePoint(): bool
+    {
+        return $this->firstPlayerScore > 3 || $this->secondPlayerScore > 3;
+    }
+
+    /**
+     * @return string
+     */
+    private function lookUpScore(): string
+    {
+        return $this->scoreText[$this->firstPlayerScore] . ' ' . $this->scoreText[$this->secondPlayerScore];
+    }
+
+    /**
+     * @return bool
+     */
+    private function isScoreDifferent(): bool
+    {
+        return $this->firstPlayerScore != $this->secondPlayerScore;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isDeuce(): bool
+    {
+        return $this->firstPlayerScore >= 3;
+    }
+
+    /**
+     * @return string
+     */
+    private function sameScore(): string
+    {
+        return $this->scoreText[$this->firstPlayerScore] . ' All';
+    }
+
+    /**
+     * @return string
+     */
+    private function deuce(): string
+    {
+        return 'Deuce';
+    }
+
 }
--- TennisTest.php
+++ TennisTest.php
@@ -6,14 +6,129 @@ require_once __DIR__ . '/Tennis.php';
 
 class TennisTest extends TestCase
 {
+    private $tennis;
+
+    protected function setUp(): void
+    {
+        $this->tennis = new Tennis('Ken', 'Tom');
+    }
+
+    private function scoreShouldBe($result): void
+    {
+        $expected = $this->tennis->getResult();;
+        $this->assertEquals($expected, $result);
+    }
+
     /**
      * @test
      */
-    public function it_should_Love_All()
+    public function game_is_Love_All()
     {
-        $tennis = new Tennis();
-        $expected = $tennis->getResult();;
-        $this->assertEquals($expected, 'Love All');
+        $this->scoreShouldBe('Love All');
     }
 
+    /**
+     * @test
+     */
+    public function game_is_Fifteen_Love()
+    {
+        $this->tennis->givenFirstPlayerScoreTimes(1);
+        $this->scoreShouldBe('Fifteen Love');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Thrity_Love()
+    {
+        $this->tennis->givenFirstPlayerScoreTimes(2);
+        $this->scoreShouldBe('Thirty Love');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Forty_Love()
+    {
+        $this->tennis->givenFirstPlayerScoreTimes(3);
+        $this->scoreShouldBe('Forty Love');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Love_Fifteen()
+    {
+        $this->tennis->givenSecondPlayerScoreTimes(1);
+        $this->scoreShouldBe('Love Fifteen');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Fifteen_All()
+    {
+        $this->tennis->givenFirstPlayerScoreTimes(1);
+        $this->tennis->givenSecondPlayerScoreTimes(1);
+        $this->scoreShouldBe('Fifteen All');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Thirty_All()
+    {
+        $this->tennis->givenFirstPlayerScoreTimes(2);
+        $this->tennis->givenSecondPlayerScoreTimes(2);
+        $this->scoreShouldBe('Thirty All');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_deuce()
+    {
+        $this->givenDeuce();
+        $this->scoreShouldBe('Deuce');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Ken_adv()
+    {
+        $this->givenDeuce();
+
+        $this->tennis->givenFirstPlayerScoreTimes(1);
+        $this->scoreShouldBe('Ken adv.');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Tom_adv()
+    {
+        $this->givenDeuce();
+        $this->tennis->givenSecondPlayerScoreTimes(1);
+        $this->scoreShouldBe('Tom adv.');
+    }
+
+    /**
+     * @test
+     */
+    public function game_is_Ken_Win()
+    {
+        $this->givenDeuce();
+        $this->tennis->givenFirstPlayerScoreTimes(2);
+        $this->scoreShouldBe('Ken Win.');
+    }
+
+
+    private function givenDeuce(): void
+    {
+        $this->tennis->givenFirstPlayerScoreTimes(3);
+        $this->tennis->givenSecondPlayerScoreTimes(3);
+    }
 }
+
+
