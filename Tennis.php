d61ff056bce9badc60716d0e08ddc38001ba7119
Ken Lin <kenlin@kkbox.com>
2021-06-19 01:44:12 +0800

    Pass, Thirty Love

--- Tennis.php
+++ Tennis.php
@@ -16,6 +16,9 @@ class Tennis
         if ($this->firstPlayerScore == 1) {
             return 'Fifteen Love';
         }
+        if ($this->firstPlayerScore == 2) {
+            return 'Thirty Love';
+        }
 
         return 'Love All';
     }
