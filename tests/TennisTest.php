816c076b6b68e3008869469dfa2e201a902b5679
Ken Lin <kenlin@kkbox.com>
2021-06-25 04:05:26 +0800

    Done, 換成q系列首次嘗試，有18m 還不錯。
    改進：
    1. 新招 cmd+shift+enter 補全[] 尾部 , 和 ; 有點神
    2. 建一個 .fori 的 posfix template
    3. 用 "$scoreTable[] Love" 取代 string split 試試

---
+++ tests/TennisTest.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Tests;
+
+use App\Tennis;
+use PHPUnit\Framework\TestCase;
+
+class TennisTest extends TestCase
+{
+    private $tennis;
+
+    /**
+     * @test
+     */
+    public function Love_All()
+    {
+        $this->scoreShouldBe('Love All');
+    }
+
+    /**
+     * @test
+     */
+    public function Fifteen_Love()
+    {
+        $this->tennis->firstPlayerGetScore();
+        $this->scoreShouldBe('Fifteen Love');
+    }
+
+    /**
+     * @test
+     */
+    public function Thirty_Love()
+    {
+        $this->givenFirstPlayerScoreTimes(2);
+        $this->scoreShouldBe('Thirty Love');
+    }
+
+    /**
+     * @test
+     */
+    public function Forty_Love()
+    {
+        $this->givenFirstPlayerScoreTimes(3);
+        $this->scoreShouldBe('Forty Love');
+    }
+
+    /**
+     * @test
+     */
+    public function Love_Fifteen()
+    {
+        $this->tennis->secondPlayerGetScore();
+        $this->scoreShouldBe('Love Fifteen');
+    }
+
+    /**
+     * @test
+     */
+    public function Love_Thirty()
+    {
+        $this->givenSecondPlayerScoreTimes(2);
+        $this->scoreShouldBe('Love Thirty');
+    }
+
+    /**
+     * @test
+     */
+    public function Love_Forty()
+    {
+        $this->givenSecondPlayerScoreTimes(3);
+        $this->scoreShouldBe('Love Forty');
+    }
+
+    /**
+     * @test
+     */
+    public function Fifteen_All()
+    {
+        $this->givenFirstPlayerScoreTimes(1);
+        $this->givenSecondPlayerScoreTimes(1);
+        $this->scoreShouldBe('Fifteen All');
+    }
+
+    /**
+     * @test
+     */
+    public function Thirty_All()
+    {
+        $this->givenFirstPlayerScoreTimes(2);
+        $this->givenSecondPlayerScoreTimes(2);
+        $this->scoreShouldBe('Thirty All');
+    }
+
+    /**
+     * @test
+     */
+    public function Deuce()
+    {
+        $this->givenDeuce();
+        $this->scoreShouldBe('Deuce');
+    }
+
+    /**
+     * @test
+     */
+    public function FirstPlayer_Adv()
+    {
+        $this->givenDeuce();
+        $this->givenFirstPlayerScoreTimes(1);
+        $this->scoreShouldBe('Ken Adv.');
+    }
+
+    /**
+     * @test
+     */
+    public function SecondPlayer_Adv()
+    {
+        $this->givenDeuce();
+        $this->givenSecondPlayerScoreTimes(1);
+        $this->scoreShouldBe('Tom Adv.');
+    }
+
+    /**
+     * @test
+     */
+    public function SecondPlayer_Win()
+    {
+        $this->givenDeuce();
+        $this->givenSecondPlayerScoreTimes(2);
+        $this->scoreShouldBe('Tom Win.');
+    }
+
+    protected function setUp(): void
+    {
+        $this->tennis = new Tennis('Ken', 'Tom');
+    }
+
+    private function scoreShouldBe($expected): void
+    {
+        $this->assertEquals($expected, $this->tennis->getScore());
+    }
+
+    private function givenFirstPlayerScoreTimes($times): void
+    {
+        for ($i = 0; $i < $times; $i++) {
+            $this->tennis->firstPlayerGetScore();
+        }
+    }
+
+    private function givenSecondPlayerScoreTimes($times): void
+    {
+        for ($i = 0; $i < $times; $i++) {
+            $this->tennis->secondPlayerGetScore();
+        }
+    }
+
+    private function givenDeuce(): void
+    {
+        $this->givenFirstPlayerScoreTimes(3);
+        $this->givenSecondPlayerScoreTimes(3);
+    }
+
+}
