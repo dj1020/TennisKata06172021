e528a0d04498fa7f9d8a4bb9960c9972751a6fa4
Ken Lin <kenlin@kkbox.com>
2021-06-28 10:44:22 +0800

    有認真比對檢討，主要是流程問題，操作倒是還好 16m50s 沒用上 z<space>

---
+++ src/Tennis.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App;
+
+class Tennis
+{
+
+    private $firstPlayerScoreTimes  = 0;
+    private $scoreTable             = [
+        0 => 'Love',
+        1 => 'Fifteen',
+        2 => 'Thirty',
+        3 => 'Forty',
+    ];
+    private $secondPlayerScoreTimes = 0;
+    private $firstPlayerName;
+    private $secondPlayerName;
+
+    /**
+     * Tennis constructor.
+     * @param $secondPlayerName
+     */
+    public function __construct($firstPlayerName, $secondPlayerName)
+    {
+        $this->firstPlayerName = $firstPlayerName;
+        $this->secondPlayerName = $secondPlayerName;
+    }
+
+    public function getScore()
+    {
+        return $this->isScoreDifferent()
+            ? ($this->isNextScoreGamePoint()
+                ? $this->advState()
+                : $this->lookupScore())
+            : ($this->isDeuce()
+                ? $this->deuce()
+                : $this->sameScore());
+
+    }
+
+    public function firstPlayerGetScore()
+    {
+        $this->firstPlayerScoreTimes++;
+    }
+
+    public function secondPlayerGetScore()
+    {
+        $this->secondPlayerScoreTimes++;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function advPlayerName()
+    {
+        $advPlayerName = $this->firstPlayerScoreTimes > $this->secondPlayerScoreTimes
+            ? $this->firstPlayerName
+            : $this->secondPlayerName;
+
+        return $advPlayerName;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isAdv(): bool
+    {
+        return abs($this->firstPlayerScoreTimes - $this->secondPlayerScoreTimes) == 1;
+    }
+
+    /**
+     * @return string
+     */
+    private function advState(): string
+    {
+        return $this->isAdv() ? $this->advPlayerName() . ' Adv.' : $this->advPlayerName() . ' Win.';
+    }
+
+    /**
+     * @return bool
+     */
+    private function isNextScoreGamePoint(): bool
+    {
+        return $this->firstPlayerScoreTimes > 3 || $this->secondPlayerScoreTimes > 3;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isScoreDifferent(): bool
+    {
+        return $this->firstPlayerScoreTimes !== $this->secondPlayerScoreTimes;
+    }
+
+    /**
+     * @return string
+     */
+    private function lookupScore(): string
+    {
+        return $this->scoreTable[$this->firstPlayerScoreTimes] . ' ' . $this->scoreTable[$this->secondPlayerScoreTimes];
+    }
+
+    /**
+     * @return bool
+     */
+    private function isDeuce(): bool
+    {
+        return $this->firstPlayerScoreTimes >= 3;
+    }
+
+    /**
+     * @return string
+     */
+    private function deuce(): string
+    {
+        return 'Deuce';
+    }
+
+    /**
+     * @return string
+     */
+    private function sameScore(): string
+    {
+        return $this->scoreTable[$this->firstPlayerScoreTimes] . ' All';
+    }
+}
